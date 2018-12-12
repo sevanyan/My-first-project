a2c766195e32751828c32cacca9eaec00d58b4bf
sevanyan <38357677+sevanyan@users.noreply.github.com>
2018-12-13 03:10:06 +0400

    Add files via upload

---
+++ event Windows Form(1)/event Windows Form/event Windows Form/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace event_Windows_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MouseDown += Form1_MouseDown;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+           if (sender is Form1)
+            {
+                Form1 form = sender as Form1;
+                Graphics g = form.CreateGraphics();
+                if (e.Button == MouseButtons.Left  )
+                {
+                    LinearGradientBrush brush = new LinearGradientBrush(e.Location, new Point(e.X + 70, e.Y + 70), Color.Red, Color.Red);
+                    g.FillEllipse(brush, e.X, e.Y, 70, 70);
+                    Pen pen = new Pen(brush, 10);
+                    pen.DashStyle = DashStyle.Solid;
+                    g.DrawRectangle(pen, e.X, e.Y, 70, 70);
+
+                }
+            }
+        }
+    }
+}
